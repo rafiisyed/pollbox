3029280e0bc3648fe4cdbbb3088de04654f07aab
thesaddamsyed <thesaddamsyed@gmail.com>
2025-08-05 19:20:00 +0530

    all the setup files provided as default

---
+++ restapi-spring-boot/src/main/java/com/poll/restapi_spring_boot/RestapiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.poll.restapi_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiSpringBootApplication.class, args);
+	}
+
+}
